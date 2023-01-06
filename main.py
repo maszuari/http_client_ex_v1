e68222ac52da79f064bc50a5ae489bc55ed6417a
Jahizah Mohammed Jabarullah <JAMOJ@orsted.com>
2023-01-06 16:38:59 +0800

    JSONDecodeError bug fixed  on 6 January 2023

--- main.py
+++ main.py
@@ -11,15 +11,26 @@ def sorted_results(out):
     for obj in sorted_list:
         print(obj)
 
-
-print('Start')
-start = datetime.datetime.now()
-conn = http.client.HTTPSConnection('test.spaceflightnewsapi.net')
-headers = {'Content-type': 'application/json'}
-conn.request("GET", "/api/v2/articles?_limit=30", headers=headers)
-response = conn.getresponse()
-js = json.loads(response.read().decode())
-sorted_results(js)
-conn.close()
-diff = datetime.datetime.now() - start
-print('End {} seconds '.format(diff.total_seconds()))
+if __name__ == "__main__":
+  print('Start')
+  start = datetime.datetime.now()
+  #print(start)
+  #conn = http.client.HTTPSConnection('test.spaceflightnewsapi.net')
+  conn = http.client.HTTPSConnection('api.spaceflightnewsapi.net')
+  headers = {'Content-type': 'application/json'}
+  #conn.request("GET", "/api/v2/articles?_limit=30", headers=headers)
+  #conn.request("GET", "api.spaceflightnewsapi.net/v3/articles", headers=headers)
+  conn.request("GET", "/v3/articles")
+  r1 = conn.getresponse()
+  print(r1.status, r1.reason)
+  #response = conn.getresponse()
+  #print(response.read().decode())
+  try:
+     js = json.loads(r1.read().decode())
+     sorted_results(js)
+     conn.close()
+     diff = datetime.datetime.now() - start
+     print('End {} seconds '.format(diff.total_seconds()))
+  except ValueError:  # includes simplejson.decoder.JSONDecodeError
+        print('Decoding JSON has failed')
+    
