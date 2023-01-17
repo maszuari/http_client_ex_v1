5c3cf497d28e4c8ea66f57824f7126cd9a3bbeec
Masz Noomu <masznoomu@gmail.com>
2023-01-17 14:57:49 +0800

    SortUtil: Add descending param in sorted_results().

--- util.py
+++ util.py
@@ -1,13 +1,15 @@
 class SortUtil:
 
-	def sorted_results(self, out):
-		'''
+	def sorted_results(self, out, descending=False):
+		"""
 		This function will sort the data.
 		param out: Python object.
-		'''
+		param descending: If True, result will be in descending order.
+		return: Return sorted list
+		"""
 		list = []
 		for obj in out:
 			list.append(obj['title'])
 		
-		sorted_list = sorted(list, key=str.lower)
+		sorted_list = sorted(list, key=str.lower, reverse=descending)
 		return sorted_list
